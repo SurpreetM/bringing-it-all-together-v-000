bf77302ed00e893a21549ee1a2930c1b33607a0c
surpreet.mundra <surpreet.mundra@gmail.com>
2018-10-12 01:05:41 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -27,7 +27,12 @@ class Dog
   end
 
   def save
-
+    sql = <<-SQL
+      INSERT INTO dogs (name, breed)
+      VALUES (?, ?)
+      SQL
+    DB[:conn].execute(sql, self.name, self.breed)
+    @id =
   end
 
 end
