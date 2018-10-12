d2d6a17d36e669202972f2638fa2f593358aeda5
surpreet.mundra <surpreet.mundra@gmail.com>
2018-10-12 01:01:20 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -11,10 +11,17 @@ class Dog
   def self.create_table
     sql = <<-SQL
         CREATE TABLE dogs (
-          id INTEGER PRIMARY KEY,
-          name TEXT,
-          breed TEXT
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
         )
+      SQL
+    DB[:conn].execute(sql)
+  end
+
+  def drop_table
+    sql <<-SQL
+      DROP TABLE dogs
       SQL
     DB[:conn].execute(sql)
   end
